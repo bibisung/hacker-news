132ec479f912d0974d805c4c17d820a04af05f98
bibisung <schmidtiana95@gmail.com>
2021-11-14 23:27:42 +0900

    refactoring

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
-const container = document.getElementById('root')
+const container = document.getElementById("root");
 const ajax = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
-ajax.open("GET", NEWS_URL, false);
-ajax.send();
+function getData(URL) {
+  ajax.open("GET", URL, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
+}
 
-const newsFeed = JSON.parse(ajax.response);
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement("ul");
 
 window.addEventListener("hashchange", () => {
   const id = location.hash.substr(1);
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
-  const title = document.createElement('h1');
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
+  const title = document.createElement("h1");
 
   title.innerHTML = newsContent.title;
   content.appendChild(title);
 });
 
 for (let i = 0; i < 10; i++) {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} [${newsFeed[i].comments_count}]`;
-  li.appendChild(a);
-  ul.appendChild(li)
+  /* DOM API를 최소화한 코드 */
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title} [${newsFeed[i].comments_count}]
+      </a>
+    </li>
+  `;
+
+  ul.appendChild(div.firstElementChild);
+
+  // const li = document.createElement("li");
+  // const a = document.createElement("a");
+  // a.href = `#${newsFeed[i].id}`;
+  // a.innerHTML = `${newsFeed[i].title} [${newsFeed[i].comments_count}]`;
+  // li.appendChild(a);
+  // ul.appendChild(li);
 }
 
 container.appendChild(ul);
