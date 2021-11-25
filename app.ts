23f4473ec49a3a6fb2a99ab26f539ccbb70a94db
Pillsang Sung <schmidtiana95@gmail.com>
2021-11-25 11:29:36 +0900

    add content type

--- app.ts
+++ app.ts
@@ -24,7 +24,7 @@ type NewsComment = News & {
 }
 const container: HTMLElement | null = document.getElementById('root');
 const ajax: XMLHttpRequest = new XMLHttpRequest();
-const content = document.createElement('div');
+const content: HTMLDivElement = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 const store: Store = {
